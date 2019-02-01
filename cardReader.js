e4d4251c8451f32219daa9af530562a60a0c4460
Jonathan Meyer <jon@stejsoftware.com>
2019-02-01 10:12:43 -0500

    adding card reader thread

---
+++ cardReader.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const mqtt = require("mqtt");
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
+
+const port = new SerialPort(process.env.comPort, {
+  baudRate: parseInt(process.env.baudRate, 10),
+  autoOpen: false
+});
+
+const client = mqtt
+  .connect(process.env.borkerURL)
+  .on("error", error => console.log(error))
+  .on("message", (topic, message) =>
+    console.log(`[${topic}] ${message.toString()}`)
+  )
+  .on("connect", () => {
+    client.publish("reader/info", ">>> MQTT Connected <<<");
+
+    port
+      .on("open", () =>
+        client.publish("reader/info", ">>> Serial Connected <<<")
+      )
+      .pipe(new Readline({ delimiter: "\r\n" }))
+      .on("data", data => client.publish("reader/card", data));
+
+    port.open(console.error);
+  })
+  .subscribe("reader/#");
