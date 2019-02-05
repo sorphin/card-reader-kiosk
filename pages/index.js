8c3750a5a5ff26bf37621efca212ccfb15a81619
Jonathan Meyer <jon@stejsoftware.com>
2019-02-05 08:43:27 -0500

    updated styles

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,10 @@ import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 
-import "../static/style.css";
 import "bootstrap/dist/css/bootstrap-reboot.css";
 import "bootstrap/dist/css/bootstrap-grid.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "../static/style.css";
 
 const mapStateToProps = (state = {}) => ({ ...state });
 
@@ -50,7 +50,10 @@ class Index extends React.Component {
     return (
       <div>
         <Head>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
         </Head>
 
         <Container>
@@ -59,13 +62,15 @@ class Index extends React.Component {
               <Image src="static/Dover-Maker-Space2.png" />
             </Col>
           </Row>
-
           <Row>
-            <Col>
+            <Col className="yellow-title-background blue-text">
               <h1>Check-In Kiosk</h1>
-
+            </Col>
+          </Row>
+          <Row className="yellow-background">
+            <Col sm="9" md="9">
               {!this.props.card && (
-                <Card>
+                <Card style={{ width: "100%", height: "100%" }}>
                   <Card.Body>
                     <Card.Title>Please scan your badge to begin</Card.Title>
                   </Card.Body>
@@ -73,58 +78,67 @@ class Index extends React.Component {
               )}
 
               {this.props.card && !this.props.account && (
-                <div>
-                  <p>This appeares to be your first time here. Please fill out the form to register.</p>
-                  <Card>
-                    <Card.Body>
-                      <Container>
-                        <Row>
-                          <Col>
-                            <Form onSubmit={e => this.handleSubmit(e)}>
-                              <Form.Group controlId="name">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control type="text" placeholder="<<< enter your name >>>" />
-                              </Form.Group>
+                <Card style={{ width: "100%", height: "100%" }}>
+                  <Card.Body>
+                    <p>
+                      This appeares to be your first time here. Please fill out
+                      the form to register.
+                    </p>
+                    <Form onSubmit={e => this.handleSubmit(e)}>
+                      <Form.Group controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="<<< enter your name >>>"
+                        />
+                      </Form.Group>
 
-                              <Form.Group controlId="n_number">
-                                <Form.Label>N-Number</Form.Label>
-                                <Form.Control type="text" placeholder="<<< enter your N number >>>" />
-                              </Form.Group>
+                      <Form.Group controlId="n_number">
+                        <Form.Label>N-Number</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="<<< enter your N number >>>"
+                        />
+                      </Form.Group>
 
-                              <ButtonToolbar>
-                                <Button variant="primary" type="submit">
-                                  Register
-                                </Button>
-                                <Button variant="secondary" onClick={e => this.handleCancel(e)}>
-                                  Cancel
-                                </Button>
-                              </ButtonToolbar>
-                            </Form>
-                          </Col>
-                          <Col md="auto">
-                            <Card style={{ width: "18rem" }}>
-                              <Card.Body>
-                                <Card.Title>Raw Data</Card.Title>
-                                <Card.Text
-                                  style={{
-                                    display: "block",
-                                    unicodeBidi: "embed",
-                                    fontFamily: "monospace",
-                                    whiteSpace: "pre"
-                                  }}
-                                >
-                                  {JSON.stringify(this.props, null, 2)}
-                                </Card.Text>
-                              </Card.Body>
-                            </Card>
-                          </Col>
-                        </Row>
-                      </Container>
-                    </Card.Body>
-                  </Card>
-                </div>
+                      <ButtonToolbar>
+                        <Button variant="liberty-primary" type="submit">
+                          Register
+                        </Button>
+                        <Button
+                          variant="liberty-secondary"
+                          onClick={e => this.handleCancel(e)}
+                        >
+                          Cancel
+                        </Button>
+                      </ButtonToolbar>
+                    </Form>
+                  </Card.Body>
+                </Card>
               )}
             </Col>
+            <Col sm="3" md="3">
+              <Card style={{ width: "100%", height: "100%" }}>
+                <Card.Body>
+                  <Card.Title>Raw Data</Card.Title>
+                  <Card.Text
+                    style={{
+                      fontSize: "small",
+                      display: "block",
+                      unicodeBidi: "embed",
+                      fontFamily: "monospace",
+                      whiteSpace: "pre",
+                      overflow: "auto"
+                    }}
+                  >
+                    {JSON.stringify(this.props, null, 2)}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="yellow-background">
+            <Col>&nbsp;</Col>
           </Row>
         </Container>
       </div>
